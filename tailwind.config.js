7a914632fdf071d5787b1a793af8ec5d96fb07fd
Jatin Garg <jatingup2005@gmail.com>
2024-07-10 19:47:31 +0530

    intial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html"],
+  theme: {
+    extend: {
+      backgroundColor:{
+        'nav':'#ec2059',
+        'rightpoint':'#f4f4f7',
+      },
+      textColor:{
+        'nav':'#ec2059',
+      },
+    },
+  },
+  plugins: [],
+}
+
